43baa0e98887353f20ae48d0dd03ff206f46f9fc
Joey Shi <46363213+joeyshi12@users.noreply.github.com>
2021-11-25 00:06:21 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from ShapeDetector import paint
-# @Copyright 2021 102peterlee.me
+# @Copyright 2021 joeyshi.tech
 
 
 if __name__ == '__main__':
+    # @Copyright 2021 joeyshi.tech
     _paint = paint.Paint()
